6f4bc5857365d2fd6aa3ae14331a64ce5e44c71f
AbiMeGab <abbybaem@gmail.com>
2022-09-18 20:16:16 -0500

    Mejorando responsividad, Bootstrap, HTML y CSS en general.

---
+++ JS/nav.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+    barra.innerHTML += `<nav>
+    <ul><li><a href="/index.html">Homepage</a></li>
+        <li>|</li>
+        <li><a href="/pages/about.html">About me</a></li>
+        <li>|</li>
+        <li><a href="/pages/skills.html">Skills</a></li>
+        <li>|</li>
+        <li><a href="/pages/proj.html">Projects</a></li>
+        <li>|</li>
+        <li><a href="/pages/quo.html">Quotations</a></li></ul>
+        </nav>`
+});
